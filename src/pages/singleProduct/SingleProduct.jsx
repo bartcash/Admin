7b33ba77748c3e3511170ba4b5fbf934f0f8b4d4
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-15 01:01:43 +0100

    completing single page

--- src/pages/singleProduct/SingleProduct.jsx
+++ src/pages/singleProduct/SingleProduct.jsx
@@ -1,5 +1,6 @@
 import "./singleProduct.css"
 import { Sidebar, Topbar } from "../../components"
+import { pic1 } from "../../constants/images"
 
 const SingleProduct = ({title}) => {
   return (
@@ -7,7 +8,43 @@ const SingleProduct = ({title}) => {
         <Sidebar/>
       <section className="single__cont">
         <Topbar title={title}/>
-      SingleProduct
+
+        <section className="single__main">
+
+          <div className="single__owner"> 
+
+          <h4>Owner</h4>
+
+            <div className="single__owner-owner">
+
+              <img src={pic1} alt="" className="single__owner-img"/>
+
+              <p>Jerry Martins</p>
+              <span>view profile</span>
+
+            </div>
+          </div>
+
+
+          {/* <section className="single__product">
+              <div className="single__product-left">
+                <h4>Name of Product</h4>
+                <span>HP Chromebook</span>
+                <h4>Price</h4>
+                <span>$230</span>
+              </div>
+              <div className="single__product-right">
+              <h4>Category</h4>
+                <span>Electronics and Accessories</span>
+                <h4>Condition</h4>
+                <span>2 months used</span>
+              </div>
+          </section> */}
+
+        </section>
+
+
+     
       </section>
     </main>
   )
